dd02e0971bcf2f8cc91ec96a4a35a1b5793cf2f6
Moe <mbaksh@redev.co.uk>
2023-12-15 22:56:34 +0000

    Switch back to old start script with Tls12 and Google DNS

--- DotNetFramework/start.ps1
+++ DotNetFramework/start.ps1
@@ -3,10 +3,17 @@ if (-not (Test-Path Env:AZP_URL)) {
   exit 1
 }
 
-if (-not (Test-Path Env:AZP_TOKEN)) {
-  Write-Error "error: missing AZP_TOKEN environment variable"
-  exit 1
+if (-not (Test-Path Env:AZP_TOKEN_FILE)) {
+  if (-not (Test-Path Env:AZP_TOKEN)) {
+    Write-Error "error: missing AZP_TOKEN environment variable"
+    exit 1
+  }
+
+  $Env:AZP_TOKEN_FILE = "\azp\.token"
+  $Env:AZP_TOKEN | Out-File -FilePath $Env:AZP_TOKEN_FILE
 }
+
+Remove-Item Env:AZP_TOKEN
 
 if ((Test-Path Env:AZP_WORK) -and -not (Test-Path $Env:AZP_WORK)) {
   New-Item $Env:AZP_WORK -ItemType directory | Out-Null
@@ -16,7 +23,7 @@ New-Item "\azp\agent" -ItemType directory | Out-Null
 Set-Location azp
 
 # Let the agent ignore the token env variables
-$Env:VSO_AGENT_IGNORE = "AZP_TOKEN"
+$Env:VSO_AGENT_IGNORE = "AZP_TOKEN,AZP_TOKEN_FILE"
 
 Set-Location agent
 
@@ -26,7 +33,7 @@ Set-DnsClientServerAddress -InterfaceIndex 4 -ServerAddresses ("8.8.8.8","8.8.4.
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(${Env:AZP_TOKEN})"))
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
 $package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
 $packageUrl = $package[0].Value.downloadUrl
 
@@ -48,7 +55,7 @@ try
     --url "$(${Env:AZP_URL})" `
     --auth PAT `
     --sslskipcertvalidation `
-    --token "$(${Env:AZP_TOKEN})" `
+    --token "$(Get-Content ${Env:AZP_TOKEN_FILE})" `
     --pool "$(if (Test-Path Env:AZP_POOL) { ${Env:AZP_POOL} } else { 'Default' })" `
     --work "$(if (Test-Path Env:AZP_WORK) { ${Env:AZP_WORK} } else { '_work' })" `
     --replace
@@ -63,5 +70,5 @@ finally
 
   .\config.cmd remove --unattended `
     --auth PAT `
-    --token "$(${Env:AZP_TOKEN})"
+    --token "$(Get-Content ${Env:AZP_TOKEN_FILE})"
 }
